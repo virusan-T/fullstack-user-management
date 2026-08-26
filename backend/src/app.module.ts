bba27af213d088760ac1b94f19b47415d72f52c2
virusan-T <thavamviru1110@gmail.com>
2026-08-26 23:54:55 +0530

    "feat: set up user module and MongoDB integration"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string, ),
+    UsersModule,
+  ],
 })
 export class AppModule {}
