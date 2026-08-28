bcc4669fa9036fa8087d64c95751f76f6bb4c13f
virusan-T <thavamviru1110@gmail.com>
2026-08-28 18:52:53 +0530

    fix: configure backend port for render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,10 +6,12 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.enableCors({
+
+  app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
+
   app.use(cookieParser());
 
   app.useGlobalPipes(
@@ -22,20 +24,29 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('User Management API')
-    .setDescription('REST API for user registration, authentication, and account management')
+    .setDescription(
+      'REST API for user registration, authentication, and account management',
+    )
     .setVersion('1.0')
-    .addBearerAuth({
+    .addBearerAuth(
+      {
         type: 'http',
         scheme: 'bearer',
         bearerFormat: 'JWT',
       },
-      'access-token',)
+      'access-token',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT || 3001;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server running on port ${port}`);
 }
+
 bootstrap();
